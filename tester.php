60d7c2efff59d0ff1abb8f429e6114e320923991
Ian Johnson <ianjohnson2000@gmail.com>
2012-09-06 16:39:02 +0100

    Wrap form fields in a div and add submit button

--- tester.php
+++ tester.php
@@ -37,46 +37,46 @@ if( !isset($_POST['authMode']) ){
 // Spit out all the post as a form
 
 ?>
-<label for="instId">instId</label>
-<input name="transId" value="<?= $id ?>" />
-<label for="cartId">cartId</label>
-<input name="cartId" value="<?= $_POST['cartId'] ?>" />
-<label for="desc">desc</label>
-<input name="desc" value="<?= $_POST['desc'] ?>" />
-<label for="amount">amount</label>
-<input name="amount" value="<?= $_POST['amount'] ?>" />
-<label for="amountString">amountString</label>
-<input name="amountString" value="<?= $amountString ?>" />
-<label for="currency">currency</label>
-<input name="currency" value="<?= $_POST['currency'] ?>" />
-<label for="authMode">authMode</label>
-<input name="authMode" value="<?= $_POST['authMode'] ?>" />
-<label for="testMode">testMode</label>
-<input name="testMode" value="<?= $_POST['testMode'] ?>" />
-<label for="name">name - required</label>
-<input name="name" value="<?= $_POST['name'] ?>" />
-<label for="address1">address1 - required</label>
-<input name="address1" value="<?= $_POST['address1'] ?>" />
-<label for="address2">address2</label>
-<input name="address2" value="<?= $_POST['address2'] ?>" />
-<label for="address3">address3</label>
-<input name="address3" value="<?= $_POST['address3'] ?>" />
-<label for="town">town</label>
-<input name="town" value="<?= $_POST['town'] ?>" />
-<label for="region">region</label>
-<input name="region" value="<?= $_POST['region'] ?>" />
-<label for="postcode">postcode</label>
-<input name="postcode" value="<?= $_POST['postcode'] ?>" />
-<label for="country">country</label>
-<input name="country" value="<?= $_POST['country'] ?>" />
-<label for="countryString">countryString</label>
-<input name="countryString" value="<?= $_POST['country'] ?>" />
-<label for="tel">tel</label>
-<input name="tel" value="<?= $_POST['tel'] ?>" />
-<label for="fax">fax</label>
-<input name="fax" value="<?= $_POST['fax'] ?>" />
-<label for="email">email</label>
-<input name="email" value="<?= $_POST['email'] ?>" />
+<div><label for="instId">instId</label>
+<input name="transId" value="<?= $_POST['instID'] ?>" /></div>
+<div><label for="cartId">cartId</label>
+<input name="cartId" value="<?= $_POST['cartId'] ?>" /></div>
+<div><label for="desc">desc</label>
+<input name="desc" value="<?= $_POST['desc'] ?>" /></div>
+<div><label for="amount">amount</label>
+<input name="amount" value="<?= $_POST['amount'] ?>" /></div>
+<div><label for="amountString">amountString</label>
+<input name="amountString" value="<?= $amountString ?>" /></div>
+<div><label for="currency">currency</label>
+<input name="currency" value="<?= $_POST['currency'] ?>" /></div>
+<div><label for="authMode">authMode</label>
+<input name="authMode" value="<?= $_POST['authMode'] ?>" /></div>
+<div><label for="testMode">testMode</label>
+<input name="testMode" value="<?= $_POST['testMode'] ?>" /></div>
+<div><label for="name">name - required</label>
+<input name="name" value="<?= $_POST['name'] ?>" /></div>
+<div><label for="address1">address1 - required</label>
+<input name="address1" value="<?= $_POST['address1'] ?>" /></div>
+<div><label for="address2">address2</label>
+<input name="address2" value="<?= $_POST['address2'] ?>" /></div>
+<div><label for="address3">address3</label>
+<input name="address3" value="<?= $_POST['address3'] ?>" /></div>
+<div><label for="town">town</label>
+<input name="town" value="<?= $_POST['town'] ?>" /></div>
+<div><label for="region">region</label>
+<input name="region" value="<?= $_POST['region'] ?>" /></div>
+<div><label for="postcode">postcode</label>
+<input name="postcode" value="<?= $_POST['postcode'] ?>" /></div>
+<div><label for="country">country</label>
+<input name="country" value="<?= $_POST['country'] ?>" /></div>
+<div><label for="countryString">countryString</label>
+<input name="countryString" value="<?= $_POST['country'] ?>" /></div>
+<div><label for="tel">tel</label>
+<input name="tel" value="<?= $_POST['tel'] ?>" /></div>
+<div><label for="fax">fax</label>
+<input name="fax" value="<?= $_POST['fax'] ?>" /></div>
+<div><label for="email">email</label>
+<input name="email" value="<?= $_POST['email'] ?>" /></div>
 
 <?php
 
@@ -84,57 +84,60 @@ if( !isset($_POST['authMode']) ){
 if( array_key_exists('withDelivery', $_POST) ){
 
 ?>
-<label for="delvName">delvName</label>
-<input name="delvName" value="<?= $_POST['delvName'] ?>" />
-<label for="delvAddress1">delvAddress1</label>
-<input name="delvAddress1" value="<?= $_POST['delvAddress1'] ?>" />
-<label for="delvAddress2">address2</label>
-<input name="delvAddress2" value="<?= $_POST['delvAddress2'] ?>" />
-<label for="delvAddress3">delvAddress3</label>
-<input name="delvAddress3" value="<?= $_POST['delvAddress3'] ?>" />
-<label for="delvTown">delvTown</label>
-<input name="delvTown" value="<?= $_POST['delvTown'] ?>" />
-<label for="delvRegion">delvRegion</label>
-<input name="delvRegion" value="<?= $_POST['delvRegion'] ?>" />
-<label for="delvPostcode">delvPostcode</label>
-<input name="delvPostcode" value="<?= $_POST['delvPostcode'] ?>" />
-<label for="delvCountry">delvCountry</label>
-<input name="delvCountry" value="<?= $_POST['delvCountry'] ?>" />
-<label for="delvCountryString">delvCountryString</label>
-<input name="delvCountryString" value="<?= $_POST['delvCountry'] ?>" />
+<div><label for="delvName">delvName</label>
+<input name="delvName" value="<?= $_POST['delvName'] ?>" /></div>
+<div><label for="delvAddress1">delvAddress1</label>
+<input name="delvAddress1" value="<?= $_POST['delvAddress1'] ?>" /></div>
+<div><label for="delvAddress2">address2</label>
+<input name="delvAddress2" value="<?= $_POST['delvAddress2'] ?>" /></div>
+<div><label for="delvAddress3">delvAddress3</label>
+<input name="delvAddress3" value="<?= $_POST['delvAddress3'] ?>" /></div>
+<div><label for="delvTown">delvTown</label>
+<input name="delvTown" value="<?= $_POST['delvTown'] ?>" /></div>
+<div><label for="delvRegion">delvRegion</label>
+<input name="delvRegion" value="<?= $_POST['delvRegion'] ?>" /></div>
+<div><label for="delvPostcode">delvPostcode</label>
+<input name="delvPostcode" value="<?= $_POST['delvPostcode'] ?>" /></div>
+<div><label for="delvCountry">delvCountry</label>
+<input name="delvCountry" value="<?= $_POST['delvCountry'] ?>" /></div>
+<div><label for="delvCountryString">delvCountryString</label>
+<input name="delvCountryString" value="<?= $_POST['delvCountry'] ?>" /></div>
 <?php
 
 }
 
 ?>
 
-<label for="compName">compName</label>
-<input name="compName" value="<?= $_POST['compName'] ?>" />
+<div><label for="compName">compName</label>
+<input name="compName" value="<?= $_POST['compName'] ?>" /></div>
 
 <h2>Transaction Results</h2>
 
-<label for="transId">transId - Generated by WorldPay Tester</label>
-<input name="transId" value="<?= $id ?>" />
-<label for="transStatus">transStatus</label>
+<div><label for="transId">transId - Generated by WorldPay Tester</label>
+<input name="transId" value="<?= $id ?>" /></div>
+<div><label for="transStatus">transStatus</label>
 <select name="transStatus">
 	<option value="Y">Y - Successful Payment</option>
 	<option value="C">C - Cancelled Payment</option>
 </select>
-<label for="transTime">transTime</label>
-<input name="transTime" value="<?= time() ?>" />
-<label for="authAmount">authAmount</label>
-<input name="authAmount" value="<?= $_POST['amount'] ?>" />
-<label for="authCurrency">authCurrency</label>
-<input name="authCurrency" value="<?= $_POST['currency'] ?>" />
-<label for="authAmountString">authAmountString</label>
-<input name="authAmountString" value="<?= $amountString ?>" />
-<label for="rawAuthMessage">rawAuthMessage</label>
+</div>
+<div><label for="transTime">transTime</label>
+<input name="transTime" value="<?= time() ?>" /></div>
+<div><label for="authAmount">authAmount</label>
+<input name="authAmount" value="<?= $_POST['amount'] ?>" /></div>
+<div><label for="authCurrency">authCurrency</label>
+<input name="authCurrency" value="<?= $_POST['currency'] ?>" /></div>
+<div><label for="authAmountString">authAmountString</label>
+<input name="authAmountString" value="<?= $amountString ?>" /></div>
+<div><label for="rawAuthMessage">rawAuthMessage</label>
 <select name="rawAuthMessage">
 	<option value="cardbe.msg.authorised">Make Payment</option>
 	<option value="trans.cancelled">Cancel Purchase</option>
 </select>
-<label for="AVS">AVS</label>
-<input name="AVS" value="<?= $_POST['AVS'] ?>" />
-<label for="ip">ip</label>
-<input name="ip" value="<?= $ip ?>" />
+</div>
+<div><label for="AVS">AVS</label>
+<input name="AVS" value="<?= $_POST['AVS'] ?>" /></div>
+<div><label for="ip">ip</label>
+<input name="ip" value="<?= $ip ?>" /></div>
+<div><input type="submit" value="Submit"/></div>
 </body>
